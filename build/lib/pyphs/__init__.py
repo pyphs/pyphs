d4fea3e3c251764196c29a2a68d1e5047da0f424
Antoine FALAIZE <antoine.falaize@gmail.com>
2016-11-12 10:22:00 +0100

    version 0.1.8

---
+++ build/lib/pyphs/__init__.py
@@ -0,0 +1,5 @@
+from pyphs import __version__, __author__, __licence__, __copyright__, \
+    PortHamiltonianObject
+
+__all__ = ['__version__', '__copyright__', '__author__', '__licence__',
+           'PortHamiltonianObject']
---
+++ build/lib/pyphs/config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 10:57:32 2016
+
+@author: Falaize
+"""
+

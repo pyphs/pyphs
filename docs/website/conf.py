345a146d8924c8fe1a59faf1e704366c56f56279
Antoine FALAIZE <antoine.falaize@gmail.com>
2016-11-13 23:32:25 +0100

    Nikola auto commit.
    Nikola version: 7.7.8

--- docs/website/conf.py
+++ docs/website/conf.py
@@ -908,7 +908,21 @@ PRETTY_URLS = True
 # SCHEDULE_ALL = False
 
 # Do you want a add a Mathjax config file?
-# MATHJAX_CONFIG = ""
+MATHJAX_CONFIG = """
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\\(","\\\)"] ],
+        displayMath: [ ['$$','$$'], ["\\\[","\\\]"] ],
+        processEscapes: true
+    },
+    displayAlign: 'left', // Change this to 'center' to center equations.
+    "HTML-CSS": {
+        styles: {'.MathJax_Display': {"margin": 0}}
+    }
+});
+</script>
+"""
 
 # If you are using the compile-ipynb plugin, just add this one:
 # MATHJAX_CONFIG = """

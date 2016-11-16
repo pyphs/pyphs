ebda1088da0c5b3281f8d8e86e65f4bc6baad8f4
Antoine FALAIZE <antoine.falaize@gmail.com>
2016-11-16 16:44:59 +0100

    Nikola auto commit.
    Nikola version: 7.7.8

--- docs/website/cache/.mako.tmp/list_post.tmpl.py
+++ docs/website/cache/.mako.tmp/list_post.tmpl.py
@@ -5,10 +5,10 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1479310958.175041
+_modified_time = 1479311098.644082
 _enable_loop = True
 _template_filename = u'/Users/Falaize/anaconda/lib/python2.7/site-packages/nikola/data/themes/base/templates/list_post.tmpl'
-_template_uri = u'list_post.tmpl'
+_template_uri = 'list_post.tmpl'
 _source_encoding = 'utf-8'
 _exports = [u'content']
 

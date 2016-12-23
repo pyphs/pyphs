e869eb3e6d779de76b441715bd8ef6d0a9a825ab
Antoine FALAIZE <antoine.falaize@gmail.com>
2016-12-23 12:01:56 +0100

    PHSSImu

--- pyphs/__init__.py
+++ pyphs/__init__.py
@@ -1,5 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from pyphs import __version__, __author__, __licence__, __copyright__, \
-    PortHamiltonianObject, PHSObject, PHSCore
+    PortHamiltonianObject, PHSObject, PHSCore, PHSNetlist, PHSSimu, PHSGraph
 
 __all__ = ['__version__', '__copyright__', '__author__', '__licence__',
-           'PortHamiltonianObject', 'PHSObject', 'PHSCore']
+           'PortHamiltonianObject', 'PHSObject', 'PHSCore', 'PHSNetlist',
+           'PHSSimu', 'PHSGraph']

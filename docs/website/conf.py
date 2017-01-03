a6140e9c6f7ebff9dc7d8f9cdb2c28b765eafbf1
Antoine FALAIZE <antoine.falaize@gmail.com>
2017-01-03 16:08:38 +0100

    Nikola auto commit.
    Nikola version: 7.7.8

--- docs/website/conf.py
+++ docs/website/conf.py
@@ -138,7 +138,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "lanyon"
+THEME = "bootstrap3"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.

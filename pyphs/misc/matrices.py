e7961e32e4159d46ddab2949ffa80d73fdc63cb5
Antoine FALAIZE <antoine.falaize@gmail.com>
2017-04-16 20:10:07 +0200

    clean

--- pyphs/misc/matrices.py
+++ pyphs/misc/matrices.py
@@ -4,7 +4,6 @@ Created on Mon May 23 22:46:56 2016
 
 @author: Falaize
 """
-import sympy as sp
 import numpy as np
 
 
@@ -27,43 +26,3 @@ def get_ind_nonzeros_row(Mat, row):
     row2list = row.tolist()[0]
     nonzeros = np.nonzero(row2list)[0]
     return list(nonzeros)
-
-
-def move_col(mat, indi, indf):
-    mat = sp.Matrix(mat)
-    if indi < indf:
-        deb = sp.Matrix.hstack(mat[:, :indi], mat[:, indi+1:indf+1])
-        end = mat[:, indf+1:]
-        eli = mat[:, indi]
-        mat = sp.Matrix.hstack(deb, eli, end)
-    elif indi > indf:
-        deb = mat[:, :indf]
-        end = sp.Matrix.hstack(mat[:, indf:indi], mat[:, indi+1:])
-        eli = mat[:, indi]
-        mat = sp.Matrix.hstack(deb, eli, end)
-    return mat
-
-
-def move_row(mat, indi, indf):
-    mat = sp.Matrix(mat)
-    if indi < indf:
-        deb = sp.Matrix.vstack(mat[:indi, :], mat[indi+1:indf+1, :])
-        end = mat[indf+1:, :]
-        eli = mat[indi, :]
-        mat = sp.Matrix.vstack(deb, eli, end)
-    elif indi > indf:
-        deb = mat[:indf, :]
-        end = sp.Matrix.vstack(mat[indf:indi, :], mat[indi+1:, :])
-        eli = mat[indi, :]
-        mat = sp.Matrix.vstack(deb, eli, end)
-    return mat
-
-if __name__ is '__main__':
-    M = np.matrix(np.eye(4))
-    print(M)
-    print(get_ind_nonzeros_col(M, 2))
-    print(get_ind_nonzeros_row(M, 2))
-
-    lis = range(10)
-    mat = np.eye(4, dtype=int)
-    print(np.matrix(move_row(mat, 1, 2)))

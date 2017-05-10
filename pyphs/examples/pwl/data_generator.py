39f2e8ea62f6f535037143581b9b9a3c6b9cc164
Antoine FALAIZE <antoine.falaize@gmail.com>
2017-05-11 01:12:54 +0200

    clean pwl

--- pyphs/examples/pwl/data_generator.py
+++ pyphs/examples/pwl/data_generator.py
@@ -8,9 +8,35 @@ Created on Wed May 10 23:48:24 2017
 
 import numpy as np
 import os
+from pyphs import PHSNetlist
+from pyphs.graphs import datum
+
 x = np.linspace(-4, 4, 11)
 y = x**3
 
 data_name = 'data'
-path = os.path.join(os.getcwd(), data_name + '.txt')
-np.savetxt(path, np.vstack((x, y)))
+data_path = data_name + '.pwl'
+#path = os.path.join(os.getcwd(), data_path)
+
+np.savetxt(data_path, np.vstack((x, y)))
+
+netlist_path = os.path.join(os.getcwd(), 'pwl.net')
+netlist = PHSNetlist(netlist_path, clear=True)
+
+netlist_line = {'dictionary': 'pwl',
+                'component': 'storage',
+                'label': 'stor',
+                'arguments': {'file': repr(data_path),
+                              'ctrl': 'e',
+                              'integ': True},
+                'nodes': ('N1', 'N2')}
+netlist.add_line(netlist_line)
+
+netlist_line = {'dictionary': 'pwl',
+                'component': 'dissipative',
+                'label': '_diss',
+                'arguments': {'file': repr(data_path),
+                              'ctrl': 'f'},
+                'nodes': (datum, 'N1')}
+netlist.add_line(netlist_line)
+netlist.write()
--- pyphs/examples/pwl/pwl.py
+++ pyphs/examples/pwl/pwl.py
@@ -10,36 +10,12 @@ Uncomment below to regenerate the netlist.
 """
 
 
-from pyphs import PHSNetlist, PHSGraph
-from pyphs.graphs import datum
+from pyphs import PHSGraph
 import os
 import sympy as sp
 
 label = 'PWL'
 netlist_path = os.path.join(os.getcwd(), 'pwl.net')
-
-#netlist = PHSNetlist(netlist_path, clear=True)
-#
-#data_name = 'data'
-#data_path = os.path.join(os.getcwd(), data_name + '.txt')
-#
-#netlist_line = {'dictionary': 'pwl',
-#                'component': 'storage',
-#                'label': label + '_stor',
-#                'arguments': {'file': repr(data_path),
-#                              'ctrl': 'e',
-#                              'integ': True},
-#                'nodes': ('N1', 'N2')}
-#netlist.add_line(netlist_line)
-#
-#netlist_line = {'dictionary': 'pwl',
-#                'component': 'dissipative',
-#                'label': label + '_diss',
-#                'arguments': {'file': repr(data_path),
-#                              'ctrl': 'f'},
-#                'nodes': (datum, 'N1')}
-#netlist.add_line(netlist_line)
-#netlist.write()
 
 graph = PHSGraph(netlist=netlist_path, label=label)
 sp.plot(graph.core.H, (graph.core.x[0], -10, 10))

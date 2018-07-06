1cf05f1d15d5bec61d716a64a84e0ade21ef2753
Antoine FALAIZE <antoine.falaize@gmail.com>
2018-07-06 16:07:20 +0200

    minor changes in examples

--- pyphs/examples/dlc/dlc.py
+++ pyphs/examples/dlc/dlc.py
@@ -9,18 +9,26 @@ Created on Sat Jan 14 11:50:23 2017
 from __future__ import absolute_import, division, print_function
 
 import os
-from pyphs import Netlist, Graph
+from pyphs import Netlist
 
+# netlist is "{label}.net"
 label = 'dlc'
 
+# get folder path
 path = os.path.realpath(__file__)[:os.path.realpath(__file__).rfind(os.sep)]
 
-netlist_filename = path + os.sep + label + '.net'
+# def filename
+netlist_filename = os.path.join(path, '{0}.net'.format(label))
 
+# read in Netlist object
 netlist = Netlist(netlist_filename)
 
-graph = Graph(netlist=netlist)
+# Build Graph object
+graph = netlist.to_graph()
+
+# Build Core object
 core = graph.to_core()
+
 
 #if __name__ == '__main__':
 #

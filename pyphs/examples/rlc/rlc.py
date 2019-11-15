efa8bc6cd0cef68bfb32254a83edca43b7a8fa95
Antoine Falaize <antoine.falaize@gmail.com>
2019-11-15 22:45:44 +0100

    Implement bufferized write in H5Data (solve #135)

--- pyphs/examples/rlc/rlc.py
+++ pyphs/examples/rlc/rlc.py
@@ -21,18 +21,18 @@ netlist = phs.Netlist(netlist_filename)
 
 core = netlist.to_core()
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #
-#    from pyphs import signalgenerator
-#    import shutil
-#    import numpy as np
-#    import sympy as sp
+#     from pyphs import signalgenerator
+#     import shutil
+#     import numpy as np
+#     import sympy as sp
 #
-#    # Include the linear dissipatives in the connexion structure
-#    core.reduce_z()
+#     # Include the linear dissipatives in the connexion structure
+#     core.reduce_z()
 #
-#    # Define the simulation parameters
-#    config = {'fs': 48e3,               # Sample rate
+#     # Define the simulation parameters
+#     config = {'fs': 48e3,               # Sample rate
 #              'grad': 'theta',    # in {'discret', 'theta', 'trapez'}
 #              'theta': 0.5,             # theta-scheme for the structure
 #              'split': True,           # apply core.split_linear() beforehand
@@ -44,32 +44,33 @@ core = netlist.to_core()
 #              'lang': 'c++',     # in {'python', 'c++'}
 #              'theano': False
 #              }
-#    # Define initialization values
-#    x0 = np.zeros(core.dims.x())
-#    x0[0] = 1
-#    x0_expr = list(map(sp.sympify, x0))
-#    inits = {'x': x0_expr}
-#    simu = core.to_simulation(config=config)
 #
-#    dur = 0.01
-#    u = signalgenerator(which='sin', f0=800., tsig=dur, fs=simu.config['fs'])
+#     # Define initialization values
+#     x0 = np.zeros(core.dims.x())
+#     x0[0] = 1
+#     x0_expr = list(map(sp.sympify, x0))
+#     inits = {'x': x0_expr}
+#     simu = core.to_simulation(config=config)
 #
-#    sequ = u[:, numpy.newaxis]
+#     dur = 0.01
+#     nt=int(dur*simu.config['fs'])
+#     u = signalgenerator(which='sin', f0=800., tsig=dur, fs=simu.config['fs'])
+#     sequ = u[:, numpy.newaxis]
 #
-#    simu.init(u=sequ, nt=int(dur*simu.config['fs']))
+#     simu.init(u=sequ, nt=nt)
 #
-#    # Run the simulation
-#    simu.process()
+#     # Run the simulation
+#     simu.process()
 #
-#    simu.data.plot([('x', 0)])
+#     simu.data.plot([('x', 0)])
 #
-#    # Plots
-#    simu.data.plot_powerbal(mode='single')
-#    simu.data.plot(['u', 'x', 'y'])
+#     # Plots
+#     simu.data.plot_powerbal(mode='single')
+#     simu.data.plot(['u', 'x', 'y'])
 #
-#    # clean: delete folders 'data' and 'figures'
-#    shutil.rmtree(os.path.join(here, 'data'))
+#     # clean: delete folders 'data' and 'figures'
+#     shutil.rmtree(os.path.join(here, 'data'))
 #
-#    # clean: delete folder 'rlc'
-#    if config['lang'] == 'c++':
+#     # clean: delete folder 'rlc'
+#     if config['lang'] == 'c++':
 #        shutil.rmtree(os.path.join(here, 'rlc'))
